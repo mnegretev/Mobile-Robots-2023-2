874361de2d56bd425e1de7c28aa5a03c105f9bb9
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2023-02-23 10:48:38 -0600

    Update Slides and gitignore

--- catkin_ws/src/students/scripts/assignment03.py
+++ catkin_ws/src/students/scripts/assignment03.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 #
 # MOBILE ROBOTS - FI-UNAM, 2023-2
-<<<<<<< HEAD
-# ASSIGNMENT 03 - THE PLATFORM ROS 
-=======
 # ASSIGNMENT 03 - THE PLATFORM ROS
->>>>>>> e6c5091f98b1d8f53fec984f5445bfc60254fda3
 #
 # Instructions:
 # Write a program to move the robot forwards until the laser
