bf0d2b12a551abf09917122f5370e3a2c963769a
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2023-02-23 10:50:30 -0600

    Update Slides and gitignore

--- catkin_ws/src/students/scripts/assignment03.py
+++ catkin_ws/src/students/scripts/assignment03.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "Luisa Hernandez"
+NAME = "MARCO NEGRETE"
 
 def callback_scan(msg):
     global obstacle_detected
