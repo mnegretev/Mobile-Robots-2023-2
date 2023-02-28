4520021c80c4a9d0eff597dc180f6d15622bdd58
Flores Barragan Daniel <danielred6@@gmail.com>
2023-02-28 15:44:11 -0600

    “Tarea3”

--- catkin_ws/src/students/scripts/assignment03.py
+++ catkin_ws/src/students/scripts/assignment03.py
@@ -22,7 +22,8 @@ def callback_scan(msg):
     # Do something to detect if there is an obstacle in front of the robot.
     # Set the 'obstacle_detected' variable with True or False, accordingly.
     #
-    
+    n = int ((msg.angle_max-msg.angle_min)/ msg.angle_increment/2)
+    obstacle_detected = msg.ranges[n]<1.0
     return
 
 def main():
@@ -42,6 +43,10 @@ def main():
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
         #
+        msg_cmd_vel = Twist()
+        msg_cmd_vel.linear.x = 0 if obstacle_detected else 0.3
+        pub_cmd_vel.publish(msg_cmd_vel)
+        
         
         loop.sleep()
 
