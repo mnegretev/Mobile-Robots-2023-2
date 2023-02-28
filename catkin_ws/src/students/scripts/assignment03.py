70918c3a4813afcc42d3e796ea00a180a5807b66
BralexMtz <brayanlexis.p6@gmail.com>
2023-02-27 21:57:55 -0600

    Tarea3 plataforma ROS

--- catkin_ws/src/students/scripts/assignment03.py
+++ catkin_ws/src/students/scripts/assignment03.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "MARCO NEGRETE"
+NAME = "BRAYAN MARTINEZ"
 
 def callback_scan(msg):
     global obstacle_detected
@@ -22,7 +22,8 @@ def callback_scan(msg):
     # Do something to detect if there is an obstacle in front of the robot.
     # Set the 'obstacle_detected' variable with True or False, accordingly.
     #
-    
+    n = int((msg.anglemax−msg.anglemin)/msg.angleincrement/2)
+    obstacle_detected=msg.ranges[n]<1.0
     return
 
 def main():
@@ -42,7 +43,9 @@ def main():
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
         #
-        
+        msg_cmd_vel = Twist ()
+        msg_cmd_vel.linear.x = 0 if obstacle_detected else 0.3
+        pub_cmd_vel.publish(msg_cmd_vel)
         loop.sleep()
 
 
