29d13e1edcc573240432c0b1377a331554301a05
danigomez0524 <42504802+danigomez0524@users.noreply.github.com>
2023-02-28 07:22:08 -0600

    Tarea 3

---
+++ catkin_ws/src/students/scripts/scripts/final_project.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python3
+#
+# MOBILE ROBOTS - FI-UNAM, 2023-2
+# FINAL PROJECT - SIMPLE SERVICE ROBOT
+# 
+# Instructions:
+# Write the code necessary to make the robot to perform the following possible commands:
+# * Robot take the <pringles|drink> to the <table|kitchen>
+# You can choose where the table and kitchen are located within the map.
+# Pringles and drink are the two objects on the table used in practice 07.
+# The Robot must recognize the orders using speech recognition.
+# Entering the command by text or similar way is not allowed.
+# The Robot must announce the progress of the action using speech synthesis,
+# for example: I'm going to grab..., I'm going to navigate to ..., I arrived to..., etc.
+# Publishers and suscribers to interact with the subsystems (navigation,
+# vision, manipulation, speech synthesis and recognition) are already declared. 
+#
+
+import rospy
+import tf
+import math
+import time
+from std_msgs.msg import String, Float64MultiArray, Float64, Bool
+from nav_msgs.msg import Path
+from nav_msgs.srv import GetPlan, GetPlanRequest
+from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import Twist, PoseStamped, Pose, Point, PointStamped
+from sound_play.msg import SoundRequest
+from custom_msgs.srv import *
+from custom_msgs.msg import *
+
+NAME = "FULL NAME"
+
+#
+# Global variable 'speech_recognized' contains the last recognized sentence
+#
+def callback_recognized_speech(msg):
+    global recognized_speech, new_task, executing_task
+    recognized_speech = msg.hypothesis[0]
+    print("New command received: " + recognized_speech)
+
+#
+# Global variable 'goal_reached' is set True when the last sent navigation goal is reached
+#
+def callback_goal_reached(msg):
+    global goal_reached
+    goal_reached = msg.data
+    print("Received goal reached: " + str(goal_reached))
+
+def parse_command(cmd):
+    obj = "pringles" if "PRINGLES" in cmd else "drink"
+    loc = [8.0,8.5] if "TABLE" in cmd else [3.22, 9.72]
+    return obj, loc
+
+#
+# This function sends the goal articular position to the left arm and sleeps 2 seconds
+# to allow the arm to reach the goal position. 
+#
+def move_left_arm(q1,q2,q3,q4,q5,q6,q7):
+    global pubLaGoalPose
+    msg = Float64MultiArray()
+    msg.data.append(q1)
+    msg.data.append(q2)
+    msg.data.append(q3)
+    msg.data.append(q4)
+    msg.data.append(q5)
+    msg.data.append(q6)
+    msg.data.append(q7)
+    pubLaGoalPose.publish(msg)
+    time.sleep(2.0)
+
+#
+# This function sends the goal angular position to the left gripper and sleeps 1 second
+# to allow the gripper to reach the goal angle. 
+#
+def move_left_gripper(q):
+    global pubLaGoalGrip
+    pubLaGoalGrip.publish(q)
+    time.sleep(1.0)
+
+#
+# This function sends the goal articular position to the right arm and sleeps 2 seconds
+# to allow the arm to reach the goal position. 
+#
+def move_right_arm(q1,q2,q3,q4,q5,q6,q7):
+    global pubRaGoalPose
+    msg = Float64MultiArray()
+    msg.data.append(q1)
+    msg.data.append(q2)
+    msg.data.append(q3)
+    msg.data.append(q4)
+    msg.data.append(q5)
+    msg.data.append(q6)
+    msg.data.append(q7)
+    pubRaGoalPose.publish(msg)
+    time.sleep(2.0)
+
+#
+# This function sends the goal angular position to the right gripper and sleeps 1 second
+# to allow the gripper to reach the goal angle. 
+#
+def move_right_gripper(q):
+    global pubRaGoalGrip
+    pubRaGoalGrip.publish(q)
+    time.sleep(1.0)
+
+#
+# This function sends the goal pan-tilt angles to the head and sleeps 1 second
+# to allow the head to reach the goal position. 
+#
+def move_head(pan, tilt):
+    global pubHdGoalPose
+    msg = Float64MultiArray()
+    msg.data.append(pan)
+    msg.data.append(tilt)
+    pubHdGoalPose.publish(msg)
+    time.sleep(1.0)
+
+#
+# This function sends a linear and angular speed to the mobile base to perform
+# low-level movements. The mobile base will move at the given linear-angular speeds
+# during a time given by 't'
+#
+def move_base(linear, angular, t):
+    global pubCmdVel
+    cmd = Twist()
+    cmd.linear.x = linear
+    cmd.angular.z = angular
+    pubCmdVel.publish(cmd)
+    time.sleep(t)
+    pubCmdVel.publish(Twist())
+
+#
+# This function publishes a global goal position. This topic is subscribed by
+# pratice04 and performs path planning and tracking.
+#
+def go_to_goal_pose(goal_x, goal_y):
+    global pubGoalPose
+    goal_pose = PoseStamped()
+    goal_pose.pose.orientation.w = 1.0
+    goal_pose.pose.position.x = goal_x
+    goal_pose.pose.position.y = goal_y
+    pubGoalPose.publish(goal_pose)
+
+#
+# This function sends a text to be synthetized.
+#
+def say(text):
+    global pubSay
+    msg = SoundRequest()
+    msg.sound   = -3
+    msg.command = 1
+    msg.volume  = 1.0
+    msg.arg2    = "voice_kal_diphone"
+    msg.arg = text
+    pubSay.publish(msg)
+
+#
+# This function calls the service for calculating inverse kinematics for left arm (practice 08)
+# and returns the calculated articular position.
+#
+def calculate_inverse_kinematics_left(x,y,z,roll, pitch, yaw):
+    req_ik.x = x
+    req_ik.y = y
+    req_ik.z = z
+    req_ik.roll  = roll
+    req_ik.pitch = pitch
+    req_ik.yaw   = yaw
+    clt = rospy.ServiceProxy("/manipulation/la_inverse_kinematics", InverseKinematics)
+    resp = clt(req_ik)
+    return [resp.q1, resp.q2, resp.q3, resp.q4, resp.q5, resp.q6, resp.q7]
+
+#
+# This function calls the service for calculating inverse kinematics for right arm (practice 08)
+# and returns the calculated articular position.
+#
+def calculate_inverse_kinematics_left(x,y,z,roll, pitch, yaw):
+    req_ik = InverseKinematicsRequest()
+    req_ik.x = x
+    req_ik.y = y
+    req_ik.z = z
+    req_ik.roll  = roll
+    req_ik.pitch = pitch
+    req_ik.yaw   = yaw
+    clt = rospy.ServiceProxy("/manipulation/ra_inverse_kinematics", InverseKinematics)
+    resp = clt(req_ik)
+    return [resp.q1, resp.q2, resp.q3, resp.q4, resp.q5, resp.q6, resp.q7]
+
+#
+# Calls the service for finding object (practice 08) and returns
+# the xyz coordinates of the requested object w.r.t. "realsense_link"
+#
+def find_object(object_name):
+    clt_find_object = rospy.ServiceProxy("/vision/find_object", FindObject)
+    req_find_object = FindObjectRequest()
+    req_find_object.cloud = rospy.wait_for_message("/hardware/realsense/points", PointCloud2)
+    req_find_object.name  = object_name
+    resp = clt_find_object(req_find_object)
+    return [resp.x, resp.y, resp.z]
+
+#
+# Transforms a point xyz expressed w.r.t. source frame to the target frame
+#
+def transform_point(x,y,z, source_frame, target_frame):
+    listener = tf.TransformListener()
+    listener.waitForTransform(target_frame, source_frame, rospy.Time(), rospy.Duration(4.0))
+    obj_p = PointStamped()
+    obj_p.header.frame_id = source_frame
+    obj_p.header.stamp = rospy.Time(0)
+    obj_p.point.x, obj_p.point.y, obj_p.point.z = x,y,z
+    obj_p = listener.transformPoint(target_frame, obj_p)
+    return [obj_p.point.x, obj_p.point.y, obj_p.point.z]
+
+def main():
+    global new_task, recognized_speech, executing_task, goal_reached
+    global pubLaGoalPose, pubRaGoalPose, pubHdGoalPose, pubLaGoalGrip, pubRaGoalGrip
+    global pubGoalPose, pubCmdVel, pubSay
+    print("FINAL PROJECT - " + NAME)
+    rospy.init_node("final_project")
+    rospy.Subscriber('/hri/sp_rec/recognized', RecognizedSpeech, callback_recognized_speech)
+    rospy.Subscriber('/navigation/goal_reached', Bool, callback_goal_reached)
+    pubGoalPose = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+    pubCmdVel   = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    pubSay      = rospy.Publisher('/robotsound', SoundRequest, queue_size=10)
+    pubLaGoalPose = rospy.Publisher("/hardware/left_arm/goal_pose" , Float64MultiArray, queue_size=10);
+    pubRaGoalPose = rospy.Publisher("/hardware/right_arm/goal_pose", Float64MultiArray, queue_size=10);
+    pubHdGoalPose = rospy.Publisher("/hardware/head/goal_pose"     , Float64MultiArray, queue_size=10);
+    pubLaGoalGrip = rospy.Publisher("/hardware/left_arm/goal_gripper" , Float64, queue_size=10);
+    pubRaGoalGrip = rospy.Publisher("/hardware/right_arm/goal_gripper", Float64, queue_size=10);
+    listener = tf.TransformListener()
+    loop = rospy.Rate(10)
+    print("Waiting for services...")
+    rospy.wait_for_service('/manipulation/la_inverse_kinematics')
+    rospy.wait_for_service('/manipulation/ra_inverse_kinematics')
+    rospy.wait_for_service('/vision/find_object')
+    print("Services are now available.")
+
+    #
+    # FINAL PROJECT 
+    #
+    
+    while not rospy.is_shutdown():
+        loop.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+    
---
+++ catkin_ws/src/students/scripts/scripts/split_and_merge.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+#
+# MOBILE ROBOTS - FI-UNAM, 2023-1
+# SPLIT AND MERGE ALGORITHM
+#
+# Instructions:
+# Test different parameteres and compare the results.
+# Parameters:
+# 
+#
+
+import rospy
+import numpy
+import math
+import tf
+from sensor_msgs.msg   import LaserScan
+from geometry_msgs.msg import Twist, Point
+from visualization_msgs.msg import Marker
+
+NAME = "Najera Santamaria Isaac Israel"
+
+def adjust_line(points):
+    [xm,ym] = numpy.mean(points, 0)
+    n,d = 0,0
+    for x,y in points:
+        n += (xm - x)*(ym - y)
+        d += (ym - y)**2 - (xm - x)**2
+    theta = 0.5*math.atan2(-2*n , d)
+    rho   = xm*math.cos(theta) + ym*math.sin(theta)
+    length= numpy.linalg.norm(points[0] - points[-1])
+    return rho, theta, xm, ym, length
+
+def find_farthest_point(points, rho, theta):
+    distances = [abs(points[i][0]*math.cos(theta) + points[i][1]*math.sin(theta) - rho) for i in range(len(points))]
+    idx = numpy.argmax(distances)
+    return idx, distances[idx]
+        
+def split(points, threshold, min_points):
+    if len(points) < min_points:
+        return []
+    rho, theta, xm, ym, length = adjust_line(points)
+    idx, dist  = find_farthest_point(points, rho, theta)
+    if dist < threshold:
+        return [[rho, theta, xm, ym, length]]
+    lines1 = split(points[0:idx], threshold, min_points)
+    lines2 = split(points[idx+1:len(points)], threshold, min_points)
+    return lines1 + lines2
+
+def merge(lines, rho_tol, theta_tol):
+    if len(lines) < 2:
+        return lines
+    new_lines = []
+    for i in range(1, len(lines)):
+        rho1, theta1, xm1, ym1, length1 = lines[i]
+        rho2, theta2, xm2, ym2, length2 = lines[i-1]
+        e_rho   = abs((rho1 - rho2)/min(rho1, rho2))
+        e_theta = abs(theta1 - theta2)
+        if e_rho < rho_tol and e_theta < theta_tol:
+            new_lines.append([(rho1+rho2)/2, (theta1+theta2)/2, (xm1+xm2)/2, (ym1+ym2)/2, length1+length2])
+        else:
+            new_lines.append([rho1, theta1, xm1, ym1, length1])
+            new_lines.append([rho2, theta2, xm2, ym2, length2])
+    return new_lines
+            
+def split_and_merge(points, threshold, min_points, rho_tol, theta_tol):
+    lines = split(points, threshold, min_points)
+    lines = merge(lines, rho_tol, theta_tol)
+    return lines
+
+def get_line_markers(lines):
+    marker = Marker()
+    marker.header.frame_id = "map";
+    marker.header.stamp = rospy.Time.now();
+    marker.ns = "segmented_lines";
+    marker.id = 7;
+    marker.type = Marker.LINE_LIST;
+    marker.action = Marker.ADD;
+    marker.scale.x = 0.1;
+    marker.color.a = 1.0;
+    marker.color.r = 0.0;
+    marker.color.g = 0.0;
+    marker.color.b = 1.0;
+    for [rho, theta, xm, ym, length] in lines:
+        a  = math.cos(theta)
+        b  = math.sin(theta)
+        marker.points.append(Point(xm + length/2*(-b), ym + length/2*(a), 0.5))
+        marker.points.append(Point(xm - length/2*(-b), ym - length/2*(a), 0.5))
+    return marker
+
+def get_laser_pose():
+    global listener
+    try:
+        ([x, y, z], rot) = listener.lookupTransform('map', 'laser_link', rospy.Time(0))
+        a = 2*math.atan2(rot[2], rot[3])
+        a = a - 2*math.pi if a > math.pi else a
+        return [x, y, z, a]
+    except:
+        pass
+    return [0,0,0,0]
+
+def callback_scan(msg):
+    global obstacle_detected, pub_line_markers
+    x,y,z,a = get_laser_pose()
+    points = []
+    for i in range(len(msg.ranges)):
+        if not (math.isnan(msg.ranges[i]) or msg.ranges[i] >= msg.range_max):
+            r,theta = msg.ranges[i], i*msg.angle_increment + msg.angle_min + a
+            points.append([r*math.cos(theta) + x, r*math.sin(theta) + y])
+    points = numpy.asarray(points)
+    #
+    # TODO:
+    # Modify the following parameters and compare the results:
+    #
+    DISTANCE_THRESHOLD  = 0.1    #Distance threshold to consider a point as part of a candidate line. 
+    MIN_POINTS_COUNTING = 1       #Minimum number of points a line should contain.
+    RHO_TOLERANCE       = 0.05    #RHO and THETA error tolerance to consider two lines as one.
+    THETA_TOLERANCE     = 0.05
+    lines = split_and_merge(points, DISTANCE_THRESHOLD, MIN_POINTS_COUNTING, RHO_TOLERANCE, THETA_TOLERANCE)
+    pub_line_markers.publish(get_line_markers(lines))
+    return
+
+def main():
+    global pub_line_markers, listener
+    print("INITIALIZING SPLIT AND MERGE ALGORITHM")
+    rospy.init_node("split_and_merge")
+    rospy.Subscriber("/hardware/scan", LaserScan, callback_scan)
+    pub_line_markers = rospy.Publisher("/navigation/segmented_lines_marker", Marker, queue_size=1)
+    listener = tf.TransformListener()
+    loop = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        loop.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+    
