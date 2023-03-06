d171fd176ac3aa5706dd504f5e7e2b430beabf71
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2023-03-06 15:40:56 -0600

    Update practice files

--- catkin_ws/src/students/scripts/practice09.py
+++ catkin_ws/src/students/scripts/practice09.py
@@ -1,180 +1,79 @@
 #!/usr/bin/env python3
 #
-# MOBILE ROBOTS - UNAM, FI, 2023-2
-# PRACTICE 09 - INVERSE KINEMATICS
+# MOBILE ROBOTS - FI-UNAM, 2023-2
+# PRACTICE 09 - COLOR SEGMENTATION
 #
 # Instructions:
-# Calculate the inverse kinematics for both manipulators (left and right) given the
-# URDF descriptions and a desired configuration. Solve the inverse kinematics using
-# the Newton-Raphson method for root finding.
-# Modify only sections marked with the 'TODO' comment
+# Complete the code to estimate the position of an object 
+# given a colored point cloud using color segmentation.
 #
 
-import math
-import rospy
-import tf
-import tf.transformations as tft
 import numpy
-import urdf_parser_py.urdf
-from geometry_msgs.msg import PointStamped
-from custom_msgs.srv import *
+import cv2
+import ros_numpy
+import rospy
+import math
+from std_msgs.msg import Header
+from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import PointStamped, Point
+from custom_msgs.srv import FindObject, FindObjectResponse
 
 NAME = "FULL_NAME"
 
-def get_model_info():
-    global joints, transforms
-    robot_model = urdf_parser_py.urdf.URDF.from_parameter_server()
-    joints = {'left': [None for i in range(8)], 'right': [None for i in range(8)]}
-    transforms = {'left':[], 'right':[]}
-    for joint in robot_model.joints:
-        for i in range(1,8):
-            joints['left' ][i-1] = joint if joint.name == ('la_'+str(i)+'_joint') else joints['left' ][i-1]
-            joints['right'][i-1] = joint if joint.name == ('ra_'+str(i)+'_joint') else joints['right'][i-1]
-        joints['left' ][7] = joint if joint.name == 'la_grip_center_joint' else joints['left' ][7]
-        joints['right'][7] = joint if joint.name == 'ra_grip_center_joint' else joints['right'][7]
-    for joint in joints['left']:
-        T = tft.translation_matrix(joint.origin.xyz)
-        R = tft.euler_matrix(joint.origin.rpy[0], joint.origin.rpy[1], joint.origin.rpy[2])
-        transforms['left'].append(tft.concatenate_matrices(T,R))
-    for joint in joints['right']:
-        T = tft.translation_matrix(joint.origin.xyz)
-        R = tft.euler_matrix(joint.origin.rpy[0], joint.origin.rpy[1], joint.origin.rpy[2])
-        transforms['right'].append(tft.concatenate_matrices(T,R))
-    
-def forward_kinematics(q, Ti, Wi):
+def segment_by_color(img_bgr, points, obj_name):
     #
     # TODO:
-    # Calculate the forward kinematics given the set of seven angles 'q'
-    # You can use the following steps:
-    #     H = I   # Assing to H a 4x4 identity matrix
-    #     for all qi in q:
-    #         H = H * Ti * Ri
-    #     H = H * Ti[7]
-    #     Get xyzRPY from the resulting Homogeneous Transformation 'H'
-    # Where:
-    #     Ti are the Homogeneous Transformations from frame i to frame i-1 when joint i is at zero position
-    #     Ri are the Homogeneous Transformations with zero-translation and rotation qi around axis Wi.
-    #     Ti[7] is the final Homogeneous Transformation from gripper center to joint 7.
-    # Hints:
-    #     Use the tft.identity_matrix() function to get the 4x4 I
-    #     Use the tft.concatenate_matrices() function for multiplying Homogeneous Transformations
-    #     Use the tft.rotation_matrix() matrices Ri.
-    #     Use the tft.euler_from_matrix() function to get RPY from matrix H
-    #     Check online documentation of these functions:
-    #     http://docs.ros.org/en/jade/api/tf/html/python/transformations.html
+    # - Assign lower and upper color limits according to the requested object:
+    #   If obj_name == 'pringles': [25, 50, 50] - [35, 255, 255]
+    #   otherwise                : [10,200, 50] - [20, 255, 255]
+    # - Change color space from RGB to HSV.
+    #   Check online documentation for cv2.cvtColor function
+    # - Determine the pixels whose color is in the selected color range.
+    #   Check online documentation for cv2.inRange
+    # - Calculate the centroid of all pixels in the given color range (ball position).
+    #   Check online documentation for cv2.findNonZero and cv2.mean
+    # - Calculate the centroid of the segmented region in the cartesian space
+    #   using the point cloud 'points'. Use numpy array notation to process the point cloud data.
+    #   Example: 'points[240,320][1]' gets the 'y' value of the point corresponding to
+    #   the pixel in the center of the image.
+    # - Return a tuple of the form: [img_x, img_y, centroid_x, centroid_y, centroid_z]
+    #   where img_x, img_y are the center of the object in image coordinates and
+    #   centroid_x, y, z are the center of the object in cartesian coordinates. 
     #
-    x,y,z,R,P,Y = 0,0,0,0,0,0
-    return numpy.asarray([x,y,z,R,P,Y])
+    return [0,0,0,0,0]
 
-def jacobian(q, Ti, Wi):
-    delta_q = 0.000001
-    #
-    # TODO:
-    # Calculate the Jacobian given a kinematic description Ti and Wi
-    # where:
-    # Ti are the Homogeneous Transformations from frame i to frame i-1 when joint i is at zero position
-    # Wi are the axis of rotation of i-th joint
-    # Use the numeric approximation:   f'(x) = (f(x+delta) - f(x-delta))/(2*delta)
-    #
-    # You can do the following steps:
-    #     J = matrix of 6x7 full of zeros
-    #     q_next = [q1+delta       q2        q3   ....     q7
-    #                  q1       q2+delta     q3   ....     q7
-    #                              ....
-    #                  q1          q2        q3   ....   q7+delta]
-    #     q_prev = [q1-delta       q2        q3   ....     q7
-    #                  q1       q2-delta     q3   ....     q7
-    #                              ....
-    #                  q1          q2        q3   ....   q7-delta]
-    #     FOR i = 1,..,7:
-    #           i-th column of J = ( FK(i-th row of q_next) - FK(i-th row of q_prev) ) / (2*delta_q)
-    #     RETURN J
-    #     
-    J = numpy.asarray([[0.0 for a in q] for i in range(6)])            # J 6x7 full of zeros
-    
-    return J
-
-def inverse_kinematics_xyzrpy(x, y, z, roll, pitch, yaw, Ti, Wi):
-    pd = numpy.asarray([x,y,z,roll,pitch,yaw])  # Desired configuration
-    tolerance = 0.01
-    max_iterations = 20
-    iterations = 0
-    #
-    # TODO:
-    # Solve the IK problem given a kinematic description (Ti, Wi) and a desired configuration.
-    # where:
-    # Ti are the Homogeneous Transformations from frame i to frame i-1 when joint i is at zero position
-    # Wi are the axis of rotation of i-th joint
-    # Use the Newton-Raphson method for root finding. (Find the roots of equation FK(q) - pd = 0)
-    # You can do the following steps:
-    #
-    #    Set an initial guess for joints 'q'. Suggested: [-0.5, 0.6, 0.3, 2.0, 0.3, 0.2, 0.3]
-    #    Calculate Forward Kinematics 'p' by calling the corresponding function
-    #    Calcualte error = p - pd
-    #    Ensure orientation angles of error are in [-pi,pi]
-    #    WHILE |error| > TOL and iterations < maximum iterations:
-    #        Calculate Jacobian
-    #        Update q estimation with q = q - pseudo_inverse(J)*error
-    #        Ensure all angles q are in [-pi,pi]
-    #        Recalculate forward kinematics p
-    #        Recalculate error and ensure angles are in [-pi,pi]
-    #        Increment iterations
-    #    Return calculated q if maximum iterations were not exceeded
-    #    Otherwise, return None
-    #
-    return None
-
-def callback_la_ik_for_pose(req):
-    global transforms, joints
-    Ti = transforms['left']                               
-    Wi = [joints['left'][i].axis for i in range(len(joints['left']))]  
-    q = inverse_kinematics_xyzrpy(req.x, req.y, req.z, req.roll, req.pitch, req.yaw, Ti, Wi)
-    if q is None:
-        return None
-    resp = InverseKinematicsResponse()
-    [resp.q1, resp.q2, resp.q3, resp.q4, resp.q5, resp.q6, resp.q7] = [q[0], q[1], q[2], q[3], q[4], q[5], q[6]]
-    return resp
-
-def callback_ra_ik_for_pose(req):
-    global transforms, joints
-    Ti = transforms['right']                               
-    Wi = [joints['right'][i].axis for i in range(len(joints['right']))]  
-    q = inverse_kinematics_xyzrpy(req.x, req.y, req.z, req.roll, req.pitch, req.yaw, Ti, Wi)
-    if q is None:
-        return False
-    resp = InverseKinematicsResponse()
-    [resp.q1, resp.q2, resp.q3, resp.q4, resp.q5, resp.q6, resp.q7] = [q[0], q[1], q[2], q[3], q[4], q[5], q[6]]
-    return resp
-
-def callback_la_fk(req):
-    global transforms, joints
-    Ti = transforms['left']                               
-    Wi = [joints['left'][i].axis for i in range(len(joints['left']))]  
-    x = forward_kinematics([req.q[0], req.q[1], req.q[2], req.q[3], req.q[4], req.q[5], req.q[6]], Ti, Wi)
-    resp = ForwardKinematicsResponse()
-    [resp.x, resp.y, resp.z, resp.roll, resp.pitch, resp.yaw] = x
-    return resp
-
-def callback_ra_fk(req):
-    global transforms, joints
-    Ti = transforms['right']                               
-    Wi = [joints['right'][i].axis for i in range(len(joints['right']))]  
-    x = forward_kinematics([req.q[0], req.q[1], req.q[2], req.q[3], req.q[4], req.q[5], req.q[6]], Ti, Wi)
-    resp = ForwardKinematicsResponse()
-    [resp.x, resp.y, resp.z, resp.roll, resp.pitch, resp.yaw] = x
+def callback_find_object(req):
+    global pub_point, img_bgr
+    print("Trying to find object: " + req.name)
+    arr = ros_numpy.point_cloud2.pointcloud2_to_array(req.cloud)
+    rgb_arr = arr['rgb'].copy()
+    rgb_arr.dtype = numpy.uint32
+    r,g,b = ((rgb_arr >> 16) & 255), ((rgb_arr >> 8) & 255), (rgb_arr & 255)
+    img_bgr = cv2.merge((numpy.asarray(b,dtype='uint8'),numpy.asarray(g,dtype='uint8'),numpy.asarray(r,dtype='uint8')))
+    [r, c, x, y, z] = segment_by_color(img_bgr, arr, req.name)
+    hdr = Header(frame_id='realsense_link', stamp=rospy.Time.now())
+    pub_point.publish(PointStamped(header=hdr, point=Point(x=x, y=y, z=z)))
+    cv2.circle(img_bgr, (int(r), int(c)), 20, [0, 255, 0], thickness=3)
+    resp = FindObjectResponse()
+    resp.x, resp.y, resp.z = x, y, z
     return resp
 
 def main():
-    print("PRACTICE 09" + NAME)
-    rospy.init_node("ik_geometric")
-    get_model_info()
-    rospy.Service("/manipulation/la_inverse_kinematics", InverseKinematics, callback_la_ik_for_pose)
-    rospy.Service("/manipulation/ra_inverse_kinematics", InverseKinematics, callback_ra_ik_for_pose)
-    rospy.Service("/manipulation/la_forward_kinematics", ForwardKinematics, callback_la_fk)
-    rospy.Service("/manipulation/ra_forward_kinematics", ForwardKinematics, callback_ra_fk)
+    global pub_point, img_bgr
+    print("PRACTICE 09 - " + NAME)
+    rospy.init_node("color_segmentation")
+    rospy.Service("/vision/find_object", FindObject, callback_find_object)
+    pub_point = rospy.Publisher('/detected_object', PointStamped, queue_size=10)
+    img_bgr = numpy.zeros((480, 640, 3), numpy.uint8)
     loop = rospy.Rate(10)
     while not rospy.is_shutdown():
+        cv2.imshow("Color Segmentation", img_bgr)
+        cv2.waitKey(1)
         loop.sleep()
-
+    
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
+
