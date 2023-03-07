06333f2373182d767cc439ddb57507cf9b89d7c9
elh-7 <emanuel.lopez@ingenieria.unam.edu>
2023-03-07 14:58:09 -0600

    Práctica 01

--- catkin_ws/src/students/scripts/voronoi.py
+++ catkin_ws/src/students/scripts/voronoi.py
@@ -20,7 +20,7 @@ from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
-NAME = "FULL NAME"
+NAME = "LÓPEZ HERNANDEZ EMANUEL"
 
 def brushfire(grid_map):
     print("Executing brushfire algorithm...")
