cd98daba8d46ecfc6878f4a968a0bf24c0b318ab
AlexisAguileraValderrama <alexisav2000@hotmail.com>
2023-03-07 08:17:37 -0600

    Practica01

--- catkin_ws/src/students/scripts/voronoi.py
+++ catkin_ws/src/students/scripts/voronoi.py
@@ -20,7 +20,7 @@ from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
-NAME = "FULL NAME"
+NAME = "Aguilera Valderrama Alexis Fernando"
 
 def brushfire(grid_map):
     print("Executing brushfire algorithm...")
