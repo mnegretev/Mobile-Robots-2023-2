a8469097571c2b31fc5387ec994da6295aba1404
fcr98 <xeballos.100@gmail.com>
2023-03-09 15:02:46 -0600

    “practice3”

--- catkin_ws/src/students/scripts/practice02a.py
+++ catkin_ws/src/students/scripts/practice02a.py
@@ -15,7 +15,7 @@ from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
-NAME = "FULL_NAME"
+NAME = "Ceballos Ricardo Fernando"
 
 def get_cost_map(static_map, cost_radius):
     if cost_radius > 20:
