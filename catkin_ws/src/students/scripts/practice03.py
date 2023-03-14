4c092e72df54430fb6f2bd4393537a0863467d12
KatherineReAl <kathephone1805@gmail.com>
2023-03-14 12:03:19 -0600

    practica2

--- catkin_ws/src/students/scripts/practice03.py
+++ catkin_ws/src/students/scripts/practice03.py
@@ -18,7 +18,7 @@ from nav_msgs.srv import GetPlan, GetPlanRequest
 from custom_msgs.srv import SmoothPath, SmoothPathRequest
 from geometry_msgs.msg import Twist, PoseStamped, Pose, Point
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Reyes Alonso"
 
 pub_goal_reached = None
 pub_cmd_vel = None
