048c9cbb823fde62c12f378fa9a9421b55d28b38
Iván Adrián Farfán <36399265+Ivanfarfan@users.noreply.github.com>
2023-03-16 03:52:19 -0600

    Update practice04.py

--- catkin_ws/src/students/scripts/practice04.py
+++ catkin_ws/src/students/scripts/practice04.py
@@ -16,7 +16,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from custom_msgs.srv import SmoothPath
 from custom_msgs.srv import SmoothPathResponse
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Farfán Canales"
 
 msg_smooth_path = Path()
 
@@ -37,7 +37,7 @@ def smooth_path(Q, alpha, beta):
     epsilon = 0.1                       
     steps   = 0
     nabla[0],nabla[1]=0,0
-    while numpy.linalg.norm(nabla)>tol*len(P) and steps<100000:
+    while numpy.linalg.norm(nabla) > tol*len(P) and steps<100000:
     
         for i in range(1,len(Q)-1):
             nabla[i]=alpha*(2*P[i]-P[i-1]-P[i+1])+beta*(P[i]-Q[i])
