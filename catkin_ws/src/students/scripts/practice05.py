456c3177eac3f89e3d3545a55bf890159ec48ee7
perry107 <josepedraza107@comunidad.unam.mx>
2023-03-27 23:23:32 -0600

    Práctica 5

--- catkin_ws/src/students/scripts/practice05.py
+++ catkin_ws/src/students/scripts/practice05.py
@@ -19,7 +19,7 @@ from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
 from sensor_msgs.msg import LaserScan
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Jose Alberto Pedraza Martinez"
 
 listener    = None
 pub_cmd_vel = None
@@ -27,12 +27,21 @@ pub_markers = None
 
 def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     cmd_vel = Twist()
+
+    alpha=0.02  #0.4 0.02
+    beta=0.02   #0.9 0.02
+    v_max=0.2
+    w_max=0.8
     #
     # TODO:
     # Implement the control law given by:
     #
     # v = v_max*math.exp(-error_a*error_a/alpha)
     # w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
+    error_a=math.atan2(goal_y-robot_y,goal_x-robot_x)-robot_a 
+    error_a=(error_a+math.pi)%(2*math.pi)-math.pi
+    v = v_max*math.exp(-error_a*error_a/alpha)
+    w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
     #
     # where error_a is the angle error and
     # v and w are the linear and angular speeds.
@@ -41,6 +50,12 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     # and return it (check online documentation for the Twist message).
     # Remember to keep error angle in the interval (-pi,pi]
     #
+    
+    cmd_vel.linear.x=v
+    cmd_vel.angular.z=w
+    
+    
+    print(v,w,error_a)
     
     return cmd_vel
 
@@ -52,7 +67,12 @@ def attraction_force(robot_x, robot_y, goal_x, goal_y):
     # where force_x and force_y are the X and Y components
     # of the resulting attraction force w.r.t. map.
     #
-    return [0, 0]
+    zeta=5
+    
+    force_x=zeta*(robot_x-goal_x)/(math.sqrt((robot_x-goal_x)**2+(robot_y-goal_y)**2))
+    force_y=zeta*(robot_y-goal_y)/(math.sqrt((robot_x-goal_x)**2+(robot_y-goal_y)**2))
+
+    return [force_x, force_y]
 
 def rejection_force(robot_x, robot_y, robot_a, laser_readings):
     #
@@ -66,8 +86,25 @@ def rejection_force(robot_x, robot_y, robot_a, laser_readings):
     # where force_x and force_y are the X and Y components
     # of the resulting rejection force w.r.t. map.
     #
+    eta=10
+    dZero=1
+    force_x=0
+    force_y=0
     
-    return [0, 0]
+    for laser_r in laser_readings:
+
+        x=math.cos(laser_r[1]+robot_a)*laser_r[0] + robot_x 
+        y=math.sin(laser_r[1]+robot_a)*laser_r[0] + robot_y       
+        
+        if (math.sqrt((x-robot_x)**2+(y-robot_y)**2) < dZero):
+            
+            force_x+=eta*   (   math.sqrt(  (   1/math.sqrt((x-robot_x)**2+(y-robot_y)**2)  )   -(1/dZero)  )   )*      (   (x-robot_x)         /       (math.sqrt((x-robot_x)**2+(y-robot_y)**2))  )
+            force_y+=eta*   (   math.sqrt(  (   1/math.sqrt((x-robot_x)**2+(y-robot_y)**2)  )   -(1/dZero)  )   )*      (   (y-robot_y)         /       (math.sqrt((y-robot_y)**2+(y-robot_y)**2))  )
+            
+    force_x=force_x/len(laser_readings)
+    force_y=force_y/len(laser_readings)
+    
+    return [force_x,force_y]
 
 def callback_pot_fields_goal(msg):
     goal_x = msg.pose.position.x
