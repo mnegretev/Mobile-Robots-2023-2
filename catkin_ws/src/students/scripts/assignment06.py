7b35f7010811a583266271e1d9b39a8fd22b1bd3
fcr98 <xeballos.100@gmail.com>
2023-03-30 14:33:14 -0600

    tarea 6

--- catkin_ws/src/students/scripts/assignment06.py
+++ catkin_ws/src/students/scripts/assignment06.py
@@ -19,7 +19,7 @@ from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
 from sensor_msgs.msg import LaserScan
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Ceballos Ricardo Fernando"
 
 listener    = None
 pub_cmd_vel = None
