59694ea20926755f12fbd3c61f012c86c2eeda42
VVTWO <abraham171270@gmail.com>
2023-04-17 10:06:36 -0600

    p4

--- catkin_ws/src/students/scripts/practice05.py
+++ catkin_ws/src/students/scripts/practice05.py
@@ -19,7 +19,7 @@ from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
 from sensor_msgs.msg import LaserScan
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "VALDERRABANO VEGA ABRAHAM"
 
 listener    = None
 pub_cmd_vel = None
