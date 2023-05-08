7248206a9639cd6b0e40affb4191b1a98426feff
Iván Adrián Farfán <36399265+Ivanfarfan@users.noreply.github.com>
2023-05-07 19:13:31 -0600

    Update practice05.cpp

--- catkin_ws/src/students/src/practice05.cpp
+++ catkin_ws/src/students/src/practice05.cpp
@@ -17,7 +17,7 @@
 #include "geometry_msgs/Pose2D.h"
 #include "tf/transform_broadcaster.h"
 
-#define NOMBRE "APELLIDO_PATERNO_APELLIDO_MATERNO"
+#define NOMBRE "Farfán Canales"
 
 #define LASER_DOWNSAMPLING  10
 #define SENSOR_NOISE        0.1
