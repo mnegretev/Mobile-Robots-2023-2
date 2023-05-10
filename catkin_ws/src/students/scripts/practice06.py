e5b67a03a06a67678c242b7bed75939601c0ad2e
Mobile Robots <rios.riv.omar@gmail.com>
2023-05-10 16:32:51 -0700

    Práctica 06 - Rios Rivera Omar

--- catkin_ws/src/students/scripts/practice06.py
+++ catkin_ws/src/students/scripts/practice06.py
@@ -18,7 +18,7 @@ from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PointStamped, Point
 from custom_msgs.srv import FindObject, FindObjectResponse
 
-NAME = "FULL_NAME"
+NAME = "RIOS RIVERA OMAR"
 
 def segment_by_color(img_bgr, points, obj_name):
     #
@@ -40,7 +40,33 @@ def segment_by_color(img_bgr, points, obj_name):
     #   where img_x, img_y are the center of the object in image coordinates and
     #   centroid_x, y, z are the center of the object in cartesian coordinates. 
     #
-    return [0,0,0,0,0]
+
+    lower = [27, 50, 50] if obj_name == "pringles" else [10, 200, 50] # Lower color limit for both objects
+    upper = [35, 255, 255] if obj_name == "pringles" else [20, 255, 255] # Upper color limit for both objects
+    lower = numpy.asarray(lower) # Convert to numpy array
+    upper = numpy.asarray(upper) 
+    img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV) # Transform to HSV code
+    img_bin = cv2.inRange(img_hsv, lower, upper) # Pixel selection from color range
+    # Erosion and dilatation
+    erosion_size = 3 # Kernel size for both operations (2*n+1)
+    erosion_shape = cv2.MORPH_ELLIPSE  # Shape of kernel (circle)
+    element = cv2.getStructuringElement(erosion_shape, (2 * erosion_size + 1, 2 * erosion_size + 1)) # Construction of kernel
+    img_bin = cv2.dilate(cv2.erode(img_bin, element),element) # Opening operation 
+    cv2.imshow("bin",img_bin) # Displays B&W image of selected color range
+    # Centroids
+    idx = cv2.findNonZero(img_bin) # Pixel indexes which are not black
+    mean_img = cv2.mean(idx) # Centroid of reconginzed object in 2D image
+ 
+    counter = 0 # Counter for mean
+    xt, yt, zt = 0, 0, 0 # Coordenates in 3D space
+    
+    for [[c, r]] in idx: # Point cloud loop
+        xt, yt, zt = xt + points[r, c][0], yt + points[r, c][1], zt + points[r, c][2] # Rearrangement of columns and rows
+        counter +=1
+    
+    xt, yt, zt = xt/counter, yt/counter, zt/counter # Centroid of region in 3D space.
+
+    return [mean_img[0],mean_img[1],xt,yt,zt] # Centroid in 2D image, centroid in 3D space.
 
 def callback_find_object(req):
     global pub_point, img_bgr
