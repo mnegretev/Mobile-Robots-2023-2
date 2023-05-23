4d2941d13a25bcd8f4acce06be9580b8c95f1bd1
VVTWO <abraham171270@gmail.com>
2023-05-22 19:04:08 -0600

    P6

--- catkin_ws/src/students/scripts/practice06.py
+++ catkin_ws/src/students/scripts/practice06.py
@@ -39,40 +39,34 @@ def segment_by_color(img_bgr, points, obj_name):
     # - Return a tuple of the form: [img_x, img_y, centroid_x, centroid_y, centroid_z]
     #   where img_x, img_y are the center of the object in image coordinates and
     #   centroid_x, y, z are the center of the object in cartesian coordinates. 
+    #
+
+    lower = [27, 50, 50] if obj_name == "pringles" else [10, 200, 50] # Lower color limit for both objects
+    upper = [35, 255, 255] if obj_name == "pringles" else [20, 255, 255] # Upper color limit for both objects
+    lower = numpy.asarray(lower) # Convert to numpy array
+    upper = numpy.asarray(upper) 
+    img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV) # Transform to HSV code
+    img_bin = cv2.inRange(img_hsv, lower, upper) # Pixel selection from color range
+    # Erosion and dilatation
+    erosion_size = 3 # Kernel size for both operations (2*n+1)
+    erosion_shape = cv2.MORPH_ELLIPSE  # Shape of kernel (circle)
+    element = cv2.getStructuringElement(erosion_shape, (2 * erosion_size + 1, 2 * erosion_size + 1)) # Construction of kernel
+    img_bin = cv2.dilate(cv2.erode(img_bin, element),element) # Opening operation 
+    cv2.imshow("bin",img_bin) # Displays B&W image of selected color range
+    # Centroids
+    idx = cv2.findNonZero(img_bin) # Pixel indexes which are not black
+    mean_img = cv2.mean(idx) # Centroid of reconginzed object in 2D image
+ 
+    counter = 0 # Counter for mean
+    xt, yt, zt = 0, 0, 0 # Coordenates in 3D space
     
-    color_inf= [25,50,50] if obj_name == 'pringles' else [12,180,140]
-    color_sup= [35,255,255] if obj_name == 'pringles' else [16,220,210]
-    color_inf = numpy.asarray(color_inf)
-    color_sup = numpy.asarray(color_sup)
-
-    img_blur=cv2.GaussianBlur(img_bgr,(5,5),0) 
-    img_hsv = cv2.cvtColor(img_blur,cv2.COLOR_BGR2HSV)  
-    img_bin = cv2.inRange(img_hsv,color_inf,color_sup) 
-
+    for [[c, r]] in idx: # Point cloud loop
+        xt, yt, zt = xt + points[r, c][0], yt + points[r, c][1], zt + points[r, c][2] # Rearrangement of columns and rows
+        counter +=1
     
-    kernel = numpy.ones((5, 5), numpy.uint8)
-    img_erosion = cv2.erode(img_bin, kernel, iterations=1)
-    img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
+    xt, yt, zt = xt/counter, yt/counter, zt/counter # Centroid of region in 3D space.
 
-    idx = cv2.findNonZero(img_dilation) 
-
-    mean_img = cv2.mean(idx) 
-
-    xt=0
-    yt=0
-    zt=0
-    counter=0
-    
-   
-    for [[c,r]] in idx:
-        xt,yt,zt = xt+points[r,c][0], yt+points[r,c][1],zt+points[r,c][2]
-        counter+=1
-    xt,yt,zt = xt/counter,yt/counter,zt/counter
-
-    print(mean_img)
-    print([xt,yt,zt])
-
-    return [int(mean_img[0]),int(mean_img[1]),xt,yt,zt]
+    return [mean_img[0],mean_img[1],xt,yt,zt] # Centroid in 2D image, centroid in 3D space.
 
 def callback_find_object(req):
     global pub_point, img_bgr
