9228a03fe35962a064574f4ab4ff0e868c8d9826
AlexisAguileraValderrama <alexisav2000@hotmail.com>
2023-06-06 16:22:59 -0600

    practica 8

--- catkin_ws/src/students/scripts/practice08.py
+++ catkin_ws/src/students/scripts/practice08.py
@@ -51,6 +51,11 @@ class NeuralNetwork(object):
         # Include input x as the first output.
         #
         y = []
+        y.append(x)
+        for i in range(len(self.biases)):
+            z = numpy.dot(self.weights[i],x) + self.biases[i]
+            x= 1.0 / (1.0 + numpy.exp(-z)) # output of the current layer is the input of the next one
+            y.append(x)
         return y
 
     def backpropagate(self, x, yt):
@@ -74,7 +79,13 @@ class NeuralNetwork(object):
         #     nabla_b[-l] = delta
         #     nabla_w[-l] = delta*ylpT  where ylpT is the transpose of outputs vector of layer l-1
         #
-
+        delta = (y[-1] -yt)*y[-1]*(1-y[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = delta*y[-2].transpose()
+        for i in range(2,self.num_layers):
+            delta = numpy.dot(self.weights[-i+1].transpose(),delta)*y[-i]*(1.0 - y[-i])
+            nabla_b[-i] = delta 
+            nabla_w[-i] = numpy.dot(delta,y[-i-1].transpose())
         return nabla_w, nabla_b
 
     def update_with_batch(self, batch, eta):
@@ -114,9 +125,7 @@ class NeuralNetwork(object):
                 sys.stdout.write("\rGradient magnitude: %f            " % (self.get_gradient_mag(nabla_w, nabla_b)))
                 sys.stdout.flush()
             print("Epoch: " + str(j))
-    #
-    ### END OF CLASS
-    #
+ 
 
 
 def load_dataset(folder):
